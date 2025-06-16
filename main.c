92977791a49773cd3c0c26cfb8847bf66b10bea1
Zakaria Khourbach <zkhourba@e3r9p12.1337.ma>
2025-06-16 15:46:14 +0100

    add celing and floor)

--- main.c
+++ main.c
@@ -3,20 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalahyan <aalahyan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zkhourba <zkhourba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 09:28:06 by zkhourba          #+#    #+#             */
-/*   Updated: 2025/06/14 15:25:30 by aalahyan         ###   ########.fr       */
+/*   Updated: 2025/06/16 15:44:59 by zkhourba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./cub3d.h"
 
-void	draw(t_all_data *data)
+void	draw_floor_and_ceiling(void *img, int ceiling_color, int floor_color)
+{
+	int	y;
+	int	x;
+
+	x = 0;
+	y = 0;
+	while (y < win_height)
+	{
+		x = 0;
+		while (x < win_width)
+		{
+			if (y < win_height / 2)
+				my_mlx_pixel_put(img, x, y, ceiling_color);
+			else
+				my_mlx_pixel_put(img, x, y, floor_color);
+			x++;
+		}
+		y++;
+	}
+}
+
+void draw(t_all_data *data)
 {
 	clear_image(&data->img);
-	start_casting(&data->player, &data->img, data->map);
+	draw_floor_and_ceiling(&data->img, 0x000ff, 0xffeeaa);
+	start_casting(&data->player, &data->img, data->map,data);
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img.img, 0, 0);
+}
+static void load_one_texture(t_all_data *d, int idx, const char *path)
+{
+    t_texture *t = &d->textures[idx];
+    t->img = mlx_xpm_file_to_image(d->mlx, (char *)path,
+                                    &t->width, &t->height);
+    if (!t->img)
+        exit(printf("Error: Could not load texture %s\n", path));
+    t->data = mlx_get_data_addr(t->img,
+                                &t->bpp,
+                                &t->line_len,
+                                &t->endian);
+}
+
+void load_textures(t_all_data *d)
+{
+    load_one_texture(d, TEX_NORTH, "/mnt/homes/zkhourba/cub3D/eagle.xpm");
+    load_one_texture(d, TEX_SOUTH, "/mnt/homes/zkhourba/cub3D/eagle.xpm");
+    load_one_texture(d, TEX_EAST,  "/mnt/homes/zkhourba/cub3D/eagle.xpm");
+    load_one_texture(d, TEX_WEST,  "/mnt/homes/zkhourba/cub3D/eagle.xpm");
 }
 
 int main(int ac, char **av)
@@ -24,25 +67,28 @@ int main(int ac, char **av)
 	t_img img;
 	t_player player;
 	t_all_data data;
-
-	data.mape = parse(ac, av);
+	unsigned int *wall_textuers;
+	
+	// data.mape = parse(ac, av);
 	int map[10][10] = {
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
 		{1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 1, 1, 1, 1, 1, 0, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
 	data.mlx = mlx_init();
-	data.mlx_win = mlx_new_window(data.mlx, 10 * TAIL, 10 * TAIL, "raycasting");
-	img.img = mlx_new_image(data.mlx, 10 * TAIL, 10 * TAIL);
+	wall_textuers = malloc(sizeof(unsigned int) * (win_height * win_width));
+	data.mlx_win = mlx_new_window(data.mlx, win_width,  win_height, "raycasting");
+	img.img = mlx_new_image(data.mlx, win_width, win_height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	data.img = img;
+	load_textures(&data);
 	player_inite(&player);
 	data.player = player;
 	memcpy(data.map, map, sizeof(map));
